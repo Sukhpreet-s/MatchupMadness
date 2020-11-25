fe7fbfc3262d74d86dd6befa1f214d765040442c
Sukhpreet Singh <200393769@ba-a251-2301.student.georgianc.on.ca>
2020-11-24 20:08:07 -0500

    initial commit

---
+++ MatchupMadness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MatchupMadness
+//
+//  Created by Sukhpreet Singh on 2020-11-24.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
