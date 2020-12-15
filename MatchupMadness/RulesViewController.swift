737faf792f428c3301fc9a09aecfcbc9b44724a5
Caird89 <32108275+Caird89@users.noreply.github.com>
2020-12-15 15:33:26 -0500

    adding rules

---
+++ MatchupMadness/RulesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RulesViewController.swift
+//  MatchupMadness
+//
+//  Created by Steven Caird on 2020-12-14.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class RulesViewController: UIViewController {
+
+    @IBOutlet weak var lblRules: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lblRules.text =
+        "1) "
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
