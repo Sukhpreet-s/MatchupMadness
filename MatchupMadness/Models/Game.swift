415c42243f8d4a1fe933b71459fea3d0412bcb83
Joel Grenier <100037633@ba-a251-2310.student.georgianc.on.ca>
2020-12-15 19:24:46 -0500

    added rules label

--- MatchupMadness/Models/Game.swift
+++ MatchupMadness/Models/Game.swift
@@ -2,7 +2,7 @@
 //  Game.swift
 //  MatchupMadness
 //
-//  Created by Sukhpreet Singh on 2020-12-13.
+//  Created by Steven Caird on 2020-12-13.
 //  Copyright © 2020 Swift Project. All rights reserved.
 //
 
--- MatchupMadness/View Controllers/StatsViewController.swift
+++ MatchupMadness/View Controllers/StatsViewController.swift
@@ -2,7 +2,7 @@
 //  StatsViewController.swift
 //  MatchupMadness
 //
-//  Created by Sukhpreet Singh on 2020-12-01.
+//  Created by Joel Grenier on 2020-12-01.
 //  Copyright © 2020 Swift Project. All rights reserved.
 //
 
